69be6e0ab25d496d66b2c73d4651b4346aaa5607
fujikow <fujikow.kontarski@gmail.com>
2025-11-04 12:02:29 -0300

    Add files via upload
    Adicionado servidor 7

--- js/cadastro.js
+++ js/cadastro.js
@@ -18,7 +18,7 @@ var MAPAS = [
     'Ferea'
 ];
 
-var SERVIDORES = [1, 2, 3, 4, 5, 6, 11, 12, 14, 15, 16, 17, 19];
+var SERVIDORES = [1, 2, 3, 4, 5, 6, 7, 11, 12, 14, 15, 16, 17, 19];
 
 // ===== INICIALIZAÇÃO =====
 document.addEventListener('DOMContentLoaded', function() {
